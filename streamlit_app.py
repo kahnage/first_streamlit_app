5f57da461f849703bcfa42a98ff35d8bd6ddf3ca
uKahnage <matt.gouws@gmail.com>
2022-05-09 22:43:36 +0100

    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Pick List for fruit
-steamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the df on the page (as table)
 streamlit.dataframe(my_fruit_list)
