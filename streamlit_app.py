a3958ad1dd88efba6ca103719e072e23aa91c695
uKahnage <matt.gouws@gmail.com>
2022-05-11 23:18:02 +0100

    add another text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,3 +50,7 @@ my_cur.execute("Select * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.text("The fruit list contains:")
 streamlit.dataframe(my_data_row)
+
+# Get data from user
+fruit_choice = streamlit.text_input('What fruit would you like to add?, 'Jackfruit')
+streamlit.write('The user entered', fruit_choice)
