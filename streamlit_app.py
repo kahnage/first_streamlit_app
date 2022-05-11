ea25d1427bb9d66c9d649b6e12a759bae07431a7
uKahnage <matt.gouws@gmail.com>
2022-05-11 23:21:46 +0100

    test insert into snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,3 +54,5 @@ streamlit.dataframe(my_data_row)
 # Get data from user
 fruit_choice = streamlit.text_input('What fruit would you like to add?', 'Jackfruit')
 streamlit.write('The user entered', fruit_choice)
+
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
