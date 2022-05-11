fc49c67d475b4201d7094d430f60dc2be7b0e017
uKahnage <matt.gouws@gmail.com>
2022-05-11 14:07:06 +0100

    Output json response

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,4 @@ streamlit.dataframe(fruits_to_show)
 
 # Get Fruityvice api data
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
