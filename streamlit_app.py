7c422e606ad37a9ae7c7d4ba5c93540e03e97c27
uKahnage <matt.gouws@gmail.com>
2022-05-11 23:45:12 +0100

    Dynamic add fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,7 +73,7 @@ if streamlit.button('Get Fruit Load List'):
 
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+    my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('" + new_fruit + "')")
     return "Thanks for adding " + new_fruit
   
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
@@ -86,7 +86,7 @@ if streamlit.button('Add a Fruit to the List'):
 # fruit_choice = streamlit.text_input('What fruit would you like to add?', 'Jackfruit')
 # streamlit.write('The user entered', fruit_choice)
 
-# my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+# my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('" + fruit_choice + "')")
 
 
 # STOP REFRESHING ENTIRE PAGE
