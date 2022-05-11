7de071bb9b631ea4bafcae0591e23d058e08d223
uKahnage <matt.gouws@gmail.com>
2022-05-11 23:18:38 +0100

    Syntax Error

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,5 +52,5 @@ streamlit.text("The fruit list contains:")
 streamlit.dataframe(my_data_row)
 
 # Get data from user
-fruit_choice = streamlit.text_input('What fruit would you like to add?, 'Jackfruit')
+fruit_choice = streamlit.text_input('What fruit would you like to add?', 'Jackfruit')
 streamlit.write('The user entered', fruit_choice)
